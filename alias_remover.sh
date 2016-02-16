bfd14f7fab1b156c995da7780a3441687f9363f3
fgallegosalido <fgallegosalido@gmail.com>
2016-02-16 16:46:40 +0100

    Script to change name added

--- alias_remover.sh
+++ alias_remover.sh
@@ -6,10 +6,7 @@ if [ $# == 1 ]
 	if [ $(grep "alias $1=" $HOME/.bash_aliases | wc -l) == 1 ]
 		then
 		unalias "$1"
-		sed "/alias $1=/d" $HOME/.bash_aliases > tmp.txt
-		rm $HOME/.bash_aliases
-		cat tmp.txt > $HOME/.bash_aliases
-		rm tmp.txt
+		sed -i "/alias $1=/d" $HOME/.bash_aliases
 		echo "Alias removed succesfully"
 	else
 		echo "Error: Alias not found"

c4bcae068ce09f7eb5cad4364f6da8e91917304d
fgallegosalido <fgallegosalido@gmail.com>
2016-02-17 16:25:48 +0100

    Created name-changer script

--- name_changer.sh
+++ name_changer.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 # Script to change the name of a defined alias
+
+if [ $# -eq 2 ]
+	then
+	if [ $(cat $HOME/.bash_aliases | grep "alias $1=" | wc -l) == 1 ]
+		then
+		if type $2 &> /dev/null
+			then
+			echo "The new name is already a command"
+		else
+      unalias $1
+      sed -i "s/alias $1=/alias $2=/g" ~/.bash_aliases
+      alias $2=$(cat ~/.bash_aliases | grep "alias $2=" | cut -d"=" -f2-)
+			echo "Alias name changed succesfully"
+		fi
+	else
+    echo "Error: Alias not found"
+	fi
+else
+	echo 'Error: Sintáxis: ./name_changer.sh <old_name> <new_name>'
+fi

89e793886dff9b44cfe18eee6dc69bb855a8c41c
fgallegosalido <fgallegosalido@gmail.com>
2016-02-18 11:02:33 +0100

    Fixed tabs of the code

--- name_changer.sh
+++ name_changer.sh
@@ -6,18 +6,17 @@ if [ $# -eq 2 ]
 	if [ $(cat $HOME/.bash_aliases | grep "alias $1=" | wc -l) == 1 ]
 		then
 		if type $2 &> /dev/null
-			then
 			echo "The new name is already a command"
 		else
 			unalias $1
-      sed -i "s/alias $1=/alias $2=/" ~/.bash_aliases
+			sed -i "s/alias $1=/alias $2=/" ~/.bash_aliases
 			string=$(cat $HOME/.bash_aliases | grep "alias $2=" | cut -d"=" -f2-)
 			string=${string:1:${#string}-2}
-      alias $2="$string"
+			alias $2="$string"
 			echo "Alias name changed succesfully"
 		fi
 	else
-  	echo "Error: Alias not found"
+		echo "Error: Alias not found"
 	fi
 else
 	echo 'Error: Syntax: . ./name_changer.sh <old_name> <new_name>'
